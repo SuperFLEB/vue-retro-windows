b7edcab4c1b6733bd368ebf99d1cb1f3e216d7f2
FLEB <voterudy@voterudy.org>
2025-04-23 02:43:52 -0400

    Fix font dithering on SystemOne in Firefox (still needs to be done in EGAVGA).

--- src/themes/SystemOne/components/RwVisualMangler.vue
+++ src/themes/SystemOne/components/RwVisualMangler.vue
@@ -1,57 +1,46 @@
 <script setup lang="ts">
-const components = ["R", "G", "B"];
+import halftones from "@/assets/shades/rgbtones-2x.png";
 </script>
 
 <template>
 	<svg class="filters" width="1" height="1">
 		<defs>
-			<g id="dot50">
-				<rect x="0" y="0" width="2" height="2" />
-				<rect x="2" y="2" width="2" height="2" />
-			</g>
-			<g id="dot75">
-				<rect x="0" y="0" width="2" height="2" />
-				<rect x="4" y="2" width="2" height="2" />
-			</g>
-			<filter id="bw" x="0" y="0" width="100%" height="100%">
-				<!-- Grayscale -->
-				<feColorMatrix type='matrix' values='0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0' in="SourceGraphic" result="grayscale" />
-
-				<!-- Isolate black to black -->
-				<feComponentTransfer in="grayscale" result="isolateBlack" color-interpolation-filters="sRGB">
-					<template v-for="comp in components">
-						<component :is="'feFunc' + comp" type="discrete" tableValues="0 1 1 1"></component>
-					</template>
+			<filter id="bw" x="0" y="0" width="100%" height="100%" color-interpolation-filters="sRGB">
+				<feColorMatrix type="matrix" values="
+				0.3333 0.3333 0.3333 0 0
+				0.3333 0.3333 0.3333 0 0
+				0.3333 0.3333 0.3333 0 0
+				0 0 0 1 0" in="SourceGraphic" result="grayscale"/>
+				<feComponentTransfer in="grayscale" result="isolated">
+					<feFuncR type="discrete" tableValues="0 1 0 0 0"/>
+					<feFuncG type="discrete" tableValues="0 0 1 0 0"/>
+					<feFuncB type="discrete" tableValues="0 0 0 1 0"/>
 				</feComponentTransfer>
-
-				<feComponentTransfer in="grayscale" result="isolateLight" color-interpolation-filters="sRGB">
-					<template v-for="comp in components">
-						<component :is="'feFunc' + comp" type="discrete" tableValues="1 1 0 1"></component>
-					</template>
+				<feImage :href="halftones" width="16" height="16" result="shadesImage" />
+				<feTile in="shadesImage" result="shadesTile" />
+				<feBlend in="isolated" in2="shadesTile" mode="multiply" result="mids" />
+				<feColorMatrix type="matrix" values="
+				1 1 1 0 0
+				1 1 1 0 0
+				1 1 1 0 0
+				0 0 0 1 0" in="mids" result="midsToned"/>
+				<feComponentTransfer in="grayscale" result="highs">
+					<feFuncR type="discrete" tableValues="0 0 0 0 1"/>
+					<feFuncG type="discrete" tableValues="0 0 0 0 1"/>
+					<feFuncB type="discrete" tableValues="0 0 0 0 1"/>
 				</feComponentTransfer>
-
-				<feComponentTransfer in="grayscale" result="isolateDark" color-interpolation-filters="sRGB">
-					<template v-for="comp in components">
-						<component :is="'feFunc' + comp" type="discrete" tableValues="1 0 1 1"></component>
-					</template>
-				</feComponentTransfer>
-
-				<feComponentTransfer in="grayscale" result="isolateWhite" color-interpolation-filters="sRGB">
-					<template v-for="comp in components">
-						<component :is="'feFunc' + comp" type="discrete" tableValues="0 0 0 1"></component>
-					</template>
-				</feComponentTransfer>
-
-				<feImage xlink:href="#dot75" result="dot75" width="8" height="4" />
-				<feTile in="dot75" x="0" y="0" result="patt75" />
-				<feComposite in="isolateLight" in2="patt75" operator="in" result="A" />
-
-				<feImage xlink:href="#dot50" result="dot50" width="4" height="4" />
-				<feTile in="dot50" x="0" y="0" result="patt50" />
-				<feComposite in="isolateDark" in2="patt50" operator="in" result="B" />
-
-				<feBlend in="A" in2="B" mode="multiply" result="AB"/>
-				<feBlend in="AB" in2="isolateBlack" mode="multiply" result="ABK"/>
+				<feBlend in="midsToned" in2="highs" mode="screen" result="toned" />
+				<feComposite in="toned" in2="SourceAlpha" operator="in"/>
+			</filter>
+			<filter id="green" x="0" y="0" width="100%" height="100%" color-interpolation-filters="sRGB">
+				<feFlood flood-color="#3f3" />
+				<feBlend in2="SourceGraphic" mode="multiply" />
+				<feComposite in2="SourceAlpha" operator="in" />
+			</filter>
+			<filter id="amber" x="0" y="0" width="100%" height="100%" color-interpolation-filters="sRGB">
+				<feFlood flood-color="#f81" />
+				<feBlend in2="SourceGraphic" mode="multiply" />
+				<feComposite in2="SourceAlpha" operator="in" />
 			</filter>
 		</defs>
 	</svg>
